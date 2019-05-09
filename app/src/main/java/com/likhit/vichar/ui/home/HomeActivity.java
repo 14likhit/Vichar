c899f494d4c39a86d46bf351786c5e051125d52f
likhit <likhit@udhaar.co>
2019-05-10 01:22:07 +0530

    [login]Added Login Activity [home]Added Home Activity

---
+++ app/src/main/java/com/likhit/vichar/ui/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.likhit.vichar.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.likhit.vichar.Base.BaseActivity;
+import com.likhit.vichar.R;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/likhit/vichar/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.likhit.vichar.ui.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.likhit.vichar.Base.BaseActivity;
+import com.likhit.vichar.R;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
